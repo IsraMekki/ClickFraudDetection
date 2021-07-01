d6e1911e2a153f534288ada2a32499f5371bb2be
AmayaSB <58055977+AmayaSB@users.noreply.github.com>
2021-07-01 18:04:49 +0200

    Update ClickBeforeDisplayDetector.scala

--- src/main/scala/ClickFraudDetection/Detectors/ClickBeforeDisplayDetector.scala
+++ src/main/scala/ClickFraudDetection/Detectors/ClickBeforeDisplayDetector.scala
@@ -1,5 +1,34 @@
 package ClickFraudDetection.Detectors
 
-class ClickBeforeDisplayDetector {
+package org.myorg.quickstart
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.JsonNode
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows
+import org.apache.flink.streaming.api.windowing.time.Time
 
+case class ClickBeforeDisplayDetector( )
+
+object ClickBeforeDisplayDetector {
+
+  def build(
+             clicks: DataStream[JsonNode],
+             windowSize: Long): Unit = {
+
+    //with a restrictive time window of 2 seconds and a minimum nb of clicks per uid equal to more or less 3
+    //we get some fraudulent clicks
+
+    //Pattern 1 :
+    //on the [[Clicks]] queue, if more than 5 clicks occur from the same [[uid]] dans une tumbling window id.
+    val clicks_by_uid: DataStream[(JsonNode, Int)] = clicks
+      .map {
+        (_, 1)
+      }
+      .keyBy(d => d._1.get("uid").asText())
+      .window(TumblingEventTimeWindows.of(Time.seconds(windowSize)))
+      .reduce { (v1, v2) => (v1._1, v1._2 + v2._2) }
+    val uid_fraud_clicks = (clicks_by_uid filter (c => (c._2 >= 3) )) // in 5 seconds more than 2 clicks, i have seen 4
+
+    uid_fraud_clicks.print()
+
+  }
 }
